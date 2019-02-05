9dfa5534937fbb5f8e5fca5bf655d781bcc39385
Andreas <andreas848@hotmail.com>
2019-02-05 11:29:49 +0100

    commit 3

---
+++ parSer.js
@@ -0,0 +1,29 @@
+
+const fetch = require("node-fetch");
+
+async function serial(count) {
+ const swappiPeople = [];
+ for (let i = 1; i <= count; i++) {
+   swappiPeople.push(
+     //Observe the await
+     await fetch("http://api.icndb.com/jokes/random")
+       .then(res => { return res.json() }));
+ }
+ console.log(swappiPeople.map(p=>p.value.joke).join(", "));
+}
+async function parallel(count) {
+ const swappiPeople = [];
+ for (let i = 1; i <= count; i++) {
+   swappiPeople.push(
+     //Observe no await
+     fetch("http://api.icndb.com/jokes/random" )
+       .then(res => { return res.json() }));
+ }
+ const allEntries = await Promise.all(swappiPeople);
+ console.log(allEntries.map(p=>p.value.joke).join("\n")); 
+ }
+//Time each of the two strategies
+serial(2);
+//parallel(15);
+
+
